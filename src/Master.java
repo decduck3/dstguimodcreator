4d48f115f3c5c192c849c3a241e2adaf48330488
decduck3 <decoftheduck@gmail.com>
2020-12-24 09:51:19 +1100

    Sorting out IDEA's GUI designer

--- src/Master.java
+++ src/Master.java
@@ -1,2 +1,25 @@
+import frames.ProjectSelect;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+
 public class Master {
+    public static ProjectSelect projectSelect;
+
+    public static void main(String[] args){
+        projectSelect = new ProjectSelect();
+
+        JFrame frame = new JFrame("ProjectSelect");
+        frame.setContentPane(projectSelect.getProjectSelectPanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        DefaultTableModel model = (DefaultTableModel) projectSelect.getProjectsListTable().getModel();
+        model.addColumn("Projects:");
+        model.addColumn("Date:");
+        model.addRow(new Object[]{"Projects:", "Date:"});
+
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
